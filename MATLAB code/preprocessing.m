1f3c24fad9053011dbed185d7e96adc2135ceee6
hildobby <h.moulie@gmail.com>
2018-01-09 14:21:48 +0100

    Add files via upload

---
+++ MATLAB code/preprocessing.m
@@ -0,0 +1,46 @@
+function preprocessing(soundfile)
+
+[y,fs]=audioread(soundfile);
+t=(0:(length(y)-1))/fs;
+
+%Short Time Fourier Transform variables
+ylen = length(y);                   % length of the signal
+wlen = 1024;                        % window length (recomended to be power of 2)
+hop = wlen/4;                       % hop size (recomended to be power of 2)
+nfft = 4096;                        % number of fft points (recomended to be power of 2)
+
+% perform STFT
+[S, f, t] = stft(y, wlen, hop, nfft, fs);
+
+% define the coherent amplification of the window
+K = sum(hamming(wlen, 'periodic'))/wlen;
+
+% take the amplitude of fft(x) and scale it, so not to be a
+% function of the length of the window and its coherent amplification
+S = abs(S)/wlen/K;
+
+% correction of the DC & Nyquist component
+if rem(nfft, 2)                     % odd nfft excludes Nyquist point
+    S(2:end, :) = S(2:end, :).*2;
+else                                % even nfft includes Nyquist point
+    S(2:end-1, :) = S(2:end-1, :).*2;
+end
+
+% convert amplitude spectrum to dB (min = -120 dB)
+S = 20*log10(S + 1e-6);
+
+% plot the spectrogram
+figure(1)
+surf(t, f, S)
+shading interp
+axis tight
+box on
+view(0, 90)
+set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
+xlabel('Time, s')
+ylabel('Frequency, Hz')
+title('Amplitude spectrogram of the signal')
+
+handl = colorbar;
+set(handl, 'FontName', 'Times New Roman', 'FontSize', 14)
+ylabel(handl, 'Magnitude, dB')
