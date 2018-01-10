185684726b6b57c22dc5bbac549c2fbe538218a9
hildobby <h.moulie@gmail.com>
2018-01-10 11:42:48 +0100

    Add files via upload

--- MATLAB code/preprocessing2.m
+++ MATLAB code/preprocessing2.m
@@ -1,4 +1,4 @@
-function preprocessing(soundfile)
+function preprocessing2(soundfile)
 
 [y,fs]=audioread(soundfile);
 t=(0:(length(y)-1))/fs;
@@ -9,8 +9,34 @@ wlen = 1024;                        % window length (recomended to be power of 2
 hop = wlen/4;                       % hop size (recomended to be power of 2)
 nfft = 4096;                        % number of fft points (recomended to be power of 2)
 
+% represent x as column-vector
+y = y(:);
+
+% form a periodic hamming window
+win = hamming(wlen, 'periodic');
+
+% stft matrix estimation and preallocation
+rown = ceil((1+nfft)/2);            % calculate the total number of rows
+coln = 1+fix((length(y)-wlen)/hop);      % calculate the total number of columns
+S = zeros(rown, coln);           % form the stft matrix
+
+% initialize the signal time segment index
+indx = 0;
+
 % perform STFT
-[S, f, t] = stft(y, wlen, hop, nfft, fs);
+for col = 1:coln
+    % windowing
+    xw = y(indx+1:indx+wlen).*win;
+    
+    % FFT
+    X = fft(xw, nfft);
+    
+    % update the stft matrix
+    S(:, col) = X(1:rown);
+    
+    % update the index
+    indx = indx + hop;
+end
 
 % define the coherent amplification of the window
 K = sum(hamming(wlen, 'periodic'))/wlen;
